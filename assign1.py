152ce3130db365702d1dfcd9c90a1a398eb254b3
JayeshW7 <118608136+JayeshW7@users.noreply.github.com>
2023-07-06 19:42:56 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,8 @@
+m1 = float(input("Enter marks for m1: "))
+m2 = float(input("Enter marks for m2: "))
+m3 = float(input("Enter marks for m3: "))
+
+average = (m1 + m2 + m3) / 3
+
+print("The average of", m1, ",", m2, "and", m3, "is", average)
+
